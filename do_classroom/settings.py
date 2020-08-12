c4c0b1510804599acce63246921e766396ea799a
Mason Egger <mason@masonegger.com>
2020-08-12 08:33:23 -0500

    requiring SSL mode

--- do_classroom/settings.py
+++ do_classroom/settings.py
@@ -131,6 +131,7 @@ DATABASES = {
         "PASSWORD": os.environ.get("DB_PASSWORD"),
         "HOST": os.environ.get("DB_HOST"),
         "PORT": os.environ.get("DB_PORT"),
+        "OPTIONS": {"sslmode": "require"},
     }
 }
 
